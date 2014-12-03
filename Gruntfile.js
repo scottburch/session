f8c42f4129ab6ef42009d6e21f1765211aee8b97
Scott Burch <scott@bulldoginfo.com>
2014-12-03 08:41:26 -0800

    moving over to underscore-fn and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dynamic_mappings: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: ['**/*.js'],
+                        dest: 'build/',
+                        ext: '.min.js',
+                        extDot: 'last'
+                    }
+                ]
+            }
+//            build: {
+//                src: 'src/<%= pkg.name %>.js',
+//                dest: 'build/<%= pkg.name %>.min.js'
+//            }
+        },
+        jasmine: {
+            pivotal: {
+                src: 'src/**/*.js',
+                options: {
+                    specs: 'spec/*Spec.js',
+                    helpers: 'spec/*Helper.js',
+                    vendor: [
+                        "vendor/*.js"
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    grunt.registerTask('default', ['uglify']);
+};
